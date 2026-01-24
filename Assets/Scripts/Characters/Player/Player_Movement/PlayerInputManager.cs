c2687671fbdfd433acaa708e08cf1cf4a803cb94
jglenewi <bubbajo1127@gmail.com>
2026-01-24 15:44:42 -0500

    created player input actions  and created PlayerInputManager script

--- Assets/Scripts/Characters/Player/Player_Movement/PlayerInputManager.cs
+++ Assets/Scripts/Characters/Player/Player_Movement/PlayerInputManager.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
 
-public class PlayerInputManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+namespace Group1{
+    public class PlayerInputManager : MonoBehaviour
     {
-        
-    }
+        public static PlayerInputManager inputs;
+        PlayerControls playerControls;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        [SerializeField] Vector2 movement;
+
+        private void Awake()
+        {
+            if(inputs == null)
+            {
+                inputs = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            } 
+        }
+
+        private void OnEnable()
+        {
+            if(playerControls == null)
+            {
+                playerControls = new PlayerControls();
+
+                playerControls.PlayerMovement.Movement.performed += i => movement = i.ReadValue<Vector2>();
+            }
+
+            playerControls.Enable();
+        }
+}
 }
