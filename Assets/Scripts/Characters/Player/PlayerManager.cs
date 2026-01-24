0ee144d1a6b991d2800ae2e8f83fc6f73b9f80e4
jglenewi <bubbajo1127@gmail.com>
2026-01-24 15:57:45 -0500

    added character and player managers and added player prefab

--- Assets/Scripts/Characters/Player/PlayerManager.cs
+++ Assets/Scripts/Characters/Player/PlayerManager.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+namespace Group1{
+    public class PlayerManager : CharacterManager
     {
-        
-    }
+        protected override void Awake()
+        {
+            base.Awake();   //runs CharacterManager Awake 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+            
+        }
+    }       
 }
