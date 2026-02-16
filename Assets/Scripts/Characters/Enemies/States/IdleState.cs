75eceac145e2d0b41e80c5ce2c3df143900328b8
Shelby <sleggott@charlotte.edu>
2026-02-16 16:23:00 -0500

    -feat(navmesh): created pursue target state

--- Assets/Scripts/Characters/Enemies/States/IdleState.cs
+++ Assets/Scripts/Characters/Enemies/States/IdleState.cs
@@ -10,8 +10,7 @@ namespace Group1 {
             {
                 // RETURN THE PURSUE TARGET STATE
                 Debug.Log("Enemy AI State: Idle State: WE HAVE A TARGET");
-                SwitchState(aiCharacter, aiCharacter.pursueTarget);
-                return this;
+                return SwitchState(aiCharacter, aiCharacter.pursueTarget);
             }
             else
             {
--- Assets/Scripts/Characters/Enemies/States/PursueTargetState.cs
+++ Assets/Scripts/Characters/Enemies/States/PursueTargetState.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Group1 {
     [CreateAssetMenu(menuName = "AI/States/Pursue Target")]
@@ -6,20 +7,25 @@ namespace Group1 {
     {
         public override AIState Tick(AICharacterManager aiCharacter)
         {
-            Debug.Log("PURSUE TARGET");
-            return base.Tick(aiCharacter);
-
             // CHECK IF WE ARE PERFORMING AN ACTION (IF SO DO NOTHING UNTIL ACTION IS COMPLETE)
+            if (aiCharacter.isPerformingAction) return this;
 
             // CHECK IF OUR TARGET IS NULL, IF SO, RETURN TO IDLE
+            if (aiCharacter.aiCharacterCombatManager.currentTarget == null) return SwitchState(aiCharacter, aiCharacter.idle);
 
             // MAKE SURE OUR NAV MESH AGENT IS ACTIVE; IF ITS NOT, ENABLE IT
+            if (!aiCharacter.navMeshAgent.enabled) aiCharacter.navMeshAgent.enabled = true;
 
             // IF WE ARE WITHIN COMBAT RANGE, SWITCH TO A COMBAT STANCE STATE
 
             // IF THE TARGET IS NOT REACHABLE, RETURN HOME
 
             // PURSUE THE TARGET
+            NavMeshPath path = new NavMeshPath();
+            aiCharacter.navMeshAgent.CalculatePath(aiCharacter.aiCharacterCombatManager.currentTarget.transform.position, path);
+            aiCharacter.navMeshAgent.SetPath(path);
+
+            return this;
         }
     }
 }
