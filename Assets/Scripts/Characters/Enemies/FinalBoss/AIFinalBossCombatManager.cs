28d705324cda788cfe588fe3527f086da7066491
Shelby <sleggott@charlotte.edu>
2026-03-21 19:22:56 -0400

    -feat (final boss): created sphere AOE attack

--- Assets/Scripts/Characters/Enemies/FinalBoss/AIFinalBossCombatManager.cs
+++ Assets/Scripts/Characters/Enemies/FinalBoss/AIFinalBossCombatManager.cs
@@ -1,15 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Group1 {
     public class AIFinalBossCombatManager : AiCharacterCombatManager
     {
-        [Header("Damage")]
+        [Header("Damage Collider")]
         [SerializeField] FinalBossMeleeWeaponDamageCollider finalBossMeleeWeaponDamageCollider;
 
         [Header("Damage")]
         [SerializeField] int baseDamage = 25;
         [SerializeField] float attack01DamageModifier = 1.0f;
         [SerializeField] float attack02DamageModifier = 1.4f;
+
+        
+        [Header("Special Attacks")]
+        [SerializeField] float circleAOEDistanceInFront = 2;
+        [SerializeField] float circleAOERadius = 2;
+        [SerializeField] int circleAOEMagicDamage = 25;
+
+        [Header("Special Attack VFXs")]
+        [SerializeField] GameObject circleAOEVFX;
+
+        private void OnDrawGizmosSelected()
+        {
+            Vector3 circleAOECenterPosition = gameObject.transform.position + transform.forward * circleAOEDistanceInFront;
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere (circleAOECenterPosition, circleAOERadius);
+        }
 
         public void SetAttack01Damage()
         {
@@ -20,6 +38,45 @@ namespace Group1 {
         {
             finalBossMeleeWeaponDamageCollider.physicalDamage = baseDamage * attack02DamageModifier;
         }
+
+        public void ActivateFinalBossCircleAOE()
+        {
+            Vector3 circleAOECenterPosition = gameObject.transform.position + transform.forward * circleAOEDistanceInFront;
+
+            // SHOW VFX
+            GameObject circleAOEObject = Instantiate(circleAOEVFX, circleAOECenterPosition, Quaternion.identity);
+            circleAOEObject.transform.localScale *= 2;
+
+            // DEAL DAMAGE AND IGNORE BOSS
+            Collider[] colliders = Physics.OverlapSphere(circleAOECenterPosition, circleAOERadius, WorldUtilityManager.Instance.GetCharacterLayers());
+
+            List<CharacterManager> charactersDamaged = new List<CharacterManager>();
+
+            foreach (var collider in colliders)
+            {
+                CharacterManager character = collider.GetComponentInParent<CharacterManager>();
+
+                if (character != null && character.characterGroup != this.gameObject.GetComponent<CharacterManager>().characterGroup)
+                {
+                    if (charactersDamaged.Contains(character)) continue;
+
+                    charactersDamaged.Add(character);
+
+                    TakeDamageEffect damageEffect = Instantiate(WorldCharacterEffectsManager.instance.takeDamageEffect);
+                    damageEffect.magicDamage = circleAOEMagicDamage;
+
+                    character.characterEffectsManager.ProcessInstantEffect(damageEffect);
+                }
+            }
+
+            // DESTROY VFX
+            Destroy(circleAOEObject, 1f);
+        }
+
+        /*public void ActivateFinalBossLineAOE()
+        {
+            Collider
+        }*/
 
         public void OpenFinalBossMeleeWeaponDamageCollider()
         {
