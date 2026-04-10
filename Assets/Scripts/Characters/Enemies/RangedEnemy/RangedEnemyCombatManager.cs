feef9012fa7175b67e5d8f9e15856bed581592b5
jglenewi <bubbajo1127@gmail.com>
2026-04-10 18:21:24 -0400

    added ranged enemy animation and scripts

---
+++ Assets/Scripts/Characters/Enemies/RangedEnemy/RangedEnemyCombatManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Group1
+{
+    public class RangedEnemyCombatManager : AiCharacterCombatManager
+    {
+        [Header("Damage")]
+        [SerializeField] int baseDamage = 25;
+    }
+}
+
