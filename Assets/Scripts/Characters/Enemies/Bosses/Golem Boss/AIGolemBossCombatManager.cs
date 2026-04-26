2216220a332277c055bab333b9b8d52e6548d754
jglenewi <bubbajo1127@gmail.com>
2026-04-26 00:49:53 -0400

    fix compiler error sorry pookie (you are ass at league lol get gud lol you suck lol #adc on top)

--- Assets/Scripts/Characters/Enemies/Bosses/Golem Boss/AIGolemBossCombatManager.cs
+++ Assets/Scripts/Characters/Enemies/Bosses/Golem Boss/AIGolemBossCombatManager.cs
@@ -7,86 +7,35 @@ namespace Group1
 {
     public class AIGolemBossCombatManager : AiCharacterCombatManager
     {
-        [SerializeField] AIFinalBossCharacterManager aiGolem;
+        [Header("Character Reference")]
+        [SerializeField] AIBossCharacterManager aiGolem;
 
-        [Required]
+        [Header("Melee Punch")]
         [SerializeField] FinalBossMeleeWeaponDamageCollider finalBossMeleeWeaponDamageCollider;
 
-        [Header("Damage")]
+        [Header("Damage Values")]
         [SerializeField] int stompPhysicalDamage = 30;
         [SerializeField] int slamPhysicalDamage = 35;
 
         [Header("Stomp AOE")]
-        [Required]
-        [SerializeField] Transform stompOrigin;      
-
+        [SerializeField] Transform stompOrigin;
         [SerializeField] float stompMaxRadius = 6f;
         [SerializeField] float stompExpandDuration = 0.45f;
         [SerializeField] GameObject stompExpandVFX;
 
-
         [Header("Slam Cone AOE")]
         [SerializeField] GameObject slamStartVFX;
-        [SerializeField] GameObject slamRollingVFX;
-
         [SerializeField] float slamStartDistanceInFront = 2f;
-        [SerializeField] float slamStepForwardDistance = 2.5f;
-        [SerializeField] int slamSteps = 3;
-
-        //Medium cone width: grows 2 -> 4 -> 6
-        [SerializeField] float slamStartWidth = 2f;
-        [SerializeField] float slamEndWidth = 6f;
-
         [SerializeField] float slamWidth = 3f;
         [SerializeField] float slamHeight = 2f;
         [SerializeField] float slamLength = 4f;
 
-
         protected override void Awake()
         {
             base.Awake();
-            aiGolem = GetComponent<AIFinalBossCharacterManager>();
+            aiGolem = GetComponent<AIBossCharacterManager>();
         }
 
-        //STOMP ATTACK
-        public void ActivateGolemStompAOE()
-        {
-            if (stompOrigin == null)
-            {
-                Debug.LogWarning("Stomp Origin not assigned!");
-                return;
-            }
-
-            Vector3 center = stompOrigin.position;
-
-            // VFX
-            if (stompVFX != null)
-            {
-                GameObject vfx = Instantiate(stompVFX, center, Quaternion.identity);
-                Destroy(vfx, 2f);
-            }
-
-            Collider[] colliders = Physics.OverlapSphere(center, stompRadius, WorldUtilityManager.Instance.GetCharacterLayers());
-
-            List<CharacterManager> damagedCharacters = new List<CharacterManager>();
-
-            foreach (var col in colliders)
-            {
-                CharacterManager character = col.GetComponentInParent<CharacterManager>();
-                if (character == null) continue;
-                if (character == aiGolem) continue;
-                if (damagedCharacters.Contains(character)) continue;
-
-                damagedCharacters.Add(character);
-
-                TakeDamageEffect dmg = Instantiate(WorldCharacterEffectsManager.instance.takeDamageEffect);
-                dmg.physicalDamage = stompPhysicalDamage;
-
-                character.characterEffectsManager.ProcessInstantEffect(dmg);
-            }
-        }
-
-        //SLAM CONE ATTACK
         public void ActivateGolemSlamAOE()
         {
             Vector3 center = aiGolem.transform.position + aiGolem.transform.forward * slamStartDistanceInFront;
@@ -112,9 +61,14 @@ namespace Group1
             ApplyDamageToColliders(colliders, slamPhysicalDamage);
         }
 
-
         public void ActivateGolemStompAOE()
         {
+            if (stompOrigin == null)
+            {
+                Debug.LogWarning("Stomp Origin not assigned!");
+                return;
+            }
+
             StartCoroutine(StompExpandRoutine());
         }
 
@@ -160,6 +114,24 @@ namespace Group1
             }
         }
 
+        private void ApplyDamageToColliders(Collider[] colliders, int damage)
+        {
+            HashSet<CharacterManager> damaged = new HashSet<CharacterManager>();
+
+            foreach (var col in colliders)
+            {
+                CharacterManager character = col.GetComponentInParent<CharacterManager>();
+                if (character == null) continue;
+                if (character == aiGolem) continue;
+                if (damaged.Contains(character)) continue;
+
+                damaged.Add(character);
+
+                TakeDamageEffect dmg = Instantiate(WorldCharacterEffectsManager.instance.takeDamageEffect);
+                dmg.physicalDamage = damage;
+                character.characterEffectsManager.ProcessInstantEffect(dmg);
+            }
+        }
 
         public void OpenFinalBossMeleeWeaponDamageCollider()
         {
